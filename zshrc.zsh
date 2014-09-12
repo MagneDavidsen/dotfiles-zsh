a3caee009cc94d41c0d5d9898f3fd950f37a2949
Magne Davidsen <magne.davidsen@gmail.com>
2014-09-12 22:58:43 +0200

    removed autosuggestions and added postgres bin

--- zshrc.zsh
+++ zshrc.zsh
@@ -51,44 +51,26 @@ plugins=(git osx zshmarks)
 source $ZSH/oh-my-zsh.sh
 source $DOTFILES/aliases.zsh
 
-# User configuration
-
-export PATH=$HOME/bin:/usr/local/bin:$PATH
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch x86_64"
-
-# ssh
-# export SSH_KEY_PATH="~/.ssh/dsa_id"
-
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
-
 # boxen
 source /opt/boxen/env.sh
 
 # Setup zsh-autosuggestions
-source $DOTFILES/zsh-autosuggestions/autosuggestions.zsh
+# source $DOTFILES/zsh-autosuggestions/autosuggestions.zsh
 
 # Enable autosuggestions automatically
 zle-line-init() {
     zle autosuggest-start
 }
 zle -N zle-line-init
+
+# User configuration
+
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+
+# postgres tools
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.3/bin
+
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
